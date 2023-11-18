4ea01b2a85717421d4457dc6a29bd954fd34c177
udayxborse <udayborse777@.com>
2023-11-18 16:03:29 +0530

    changes

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <nav className='stick to top-0 bg-slate-500 mx-20 '>
+          <div>
+            <h1>
+              Welcome to uday
+            </h1>
+          </div>
+
+        </nav>
+      </div>
+    </div>
+  )
+}
